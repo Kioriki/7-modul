0cb309e1a15e0f532010d3a1ebf8ffa5fc8410db
Kioriki <dashatokcinka@gmail.com>
2023-12-12 12:08:54 +0400

    Update zadanie4.js

--- zadanie4.js
+++ zadanie4.js
@@ -4,18 +4,20 @@ function electricalAppliances(name, power, amperage, enable){
     this.amperage = amperage,
     this.enable = true
     }
-  function lightBulb (name, power, amperage, enable){
-    this.name = name,
-    this.power= power,
-    this.amperage = amperage,
-    this.enable = enable;
-    this.electrical = function(){
+
+    electricalAppliances.prototype.getEnable = function(name, power, amperage, enable){
         if (enable == 0) {
             console.log(`${this.name} Appliances: ${this.power} W and ${this.amperage} A is disabled` )
         } else {
             console.log(`${this.name} Appliances: ${this.power} W and ${this.amperage} A is enabled` )
         }
     }
+
+  function lightBulb (name, power, amperage, enable){
+    this.name = name,
+    this.power= power,
+    this.amperage = amperage,
+    this.enable = enable;
   }
   lightBulb.Prototype = new electricalAppliances()
   function telephone(name, power, amperage, enable){
@@ -23,17 +25,10 @@ function electricalAppliances(name, power, amperage, enable){
     this.power = power,
     this.amperage = amperage,
     this.enable = enable;
-    this.electrical = function(){
-        if (enable == 0) {
-            console.log(`${this.name} Appliances: ${this.power} W and ${this.amperage} A is disabled` )
-        } else {
-            console.log(`${this.name} Appliances: ${this.power} W and ${this.amperage} A is enabled` )
-        }
     }
-  }
   telephone.Prototype = new electricalAppliances()
   
   const light = new electricalAppliances('Lightbulb', 6, '0,093 A', 0);
   const phone = new electricalAppliances('Telephone', 10, '2,4 A', 1);
   
-  console.log(lightBulb())
+  light.getEnable ('Lightbulb', 6 , '0,093 A', 0)
